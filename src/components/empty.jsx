d66dae0ace906386360517fc83500e24150b3f93
Fatima-Ezzahra-Abdessettar <fatimaezzahraabdessettar@gmail.com>
2025-08-25 10:35:36 +0100

    Fix cat image and other changes

--- src/components/empty.jsx
+++ src/components/empty.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import catPic from '../assets/cat-tasks.png'
 
 const empty = () => {
   return (
     <div>
         <div className='flex flex-col justify-center items-center space-y-5 mt-20 md:mt-25'>
-            <img src="src/assets/cat-tasks.png" alt="" />
+            <img src={catPic} alt="" />
             <p className='text-xl text-input'>No tasks yet ... </p>
         </div>
     </div>
